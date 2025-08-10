c6d00af13812bb76fe5c8e526870019e0a7768af
rbegacobas <rbegacobas@gmail.com>
2025-08-10 01:06:03 -0300

    Configura Firebase en Android e iOS y verifica compilación (close #3)

--- client_app/lib/main.dart
+++ client_app/lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+
+
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -105,10 +111,12 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text('You have pushed the button this many times:'),
+            const Center(child: Text('¡Firebase inicializado!')),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            
           ],
         ),
       ),
